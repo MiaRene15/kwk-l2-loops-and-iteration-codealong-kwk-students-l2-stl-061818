a8dd86c933e77e0f4d162f93fcd31be7f5b78c02
MiaRene15 <kids.hines@icloud.com>
2018-06-26 13:58:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+ let names = []
+ 
+ function printBadges(names, names) {
+   console.log(names.push('Lisa'))
+   console.log(names.push('Kaitlin'))
+   console.log(names.push('Jan'))
+   return`Welcome ${names}! You are employee #1.`
+   return `Welcome ${names}! You are employee #2.`
+}
+ 
+//  function printBadges(names) {
+//   console.log(`${names}`)
+//   console.log(`${names}`)
+ //  console.log(`${names}`)
+ //  'Welcome names[3]! You are employee #1. '
+ //}
