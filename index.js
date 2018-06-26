f3301f33d1c0df7efe4569b3f16a924aa8eb42c9
MiaRene15 <kids.hines@icloud.com>
2018-06-26 14:04:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,20 @@
    console.log(names.push('Jan'))
    console.log('Welcome' names.push('')! )
 }
+
+function tailsNeverFails() {
+  let tails = 0; 
+    
+   while (Math.random() >= 0.01) {
+   tails++ 
+  }
+    
+ console.log(`You got ${tails} tails in a row!`);
+ }
+  
+ // for (let t = 0; t <= 100; t++) {
+ //   tailsNeverFails();
+ // }
  
 //  function printBadges(names) {
 //   console.log(`${names}`)
